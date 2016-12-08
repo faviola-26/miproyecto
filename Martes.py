c88a16e1ff07c3cadfbaa8fcf8b022ac5f0c7fa4
faviola-26 <faviola_jo@hotmail.com>
2016-12-08 17:56:34 -0600

    Agrege la clase persona y el metodo fija nombre

--- Martes.py
+++ Martes.py
@@ -3,21 +3,28 @@
 class Imagen():
     def tamarindo(self):
 	print("1,2,3 tamarindo")
-	
+
 
     def escalaDeGrises(self):
         pass
     def binarizacion(self):
         pass
-    
+
 class Sonido():
     def reproducir(Self):
 	print("Reproduciendo")
-        
+
 
     def volumen(Self):
         pass
 
     def furier(self):
         pass
-    
+
+class Persona():
+    def __init__(self, arg):
+        super(Persona, self).__init__()
+        self.arg = arg
+
+    def fijaNombre(Self):
+        print("El nombre es :v")
